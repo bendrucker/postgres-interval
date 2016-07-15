645a5a1b848c1bd7dfcfa419c7c75740f4603e36
Alexey Zabrodsky <elmigranto@gmail.com>
2016-07-15 15:53:29 +0500

    Parse and format milliseconds
    Closes #8

--- index.js
+++ index.js
@@ -13,10 +13,24 @@ function PostgresInterval (raw) {
 var properties = ['seconds', 'minutes', 'hours', 'days', 'months', 'years']
 PostgresInterval.prototype.toPostgres = function () {
   var filtered = properties.filter(this.hasOwnProperty, this)
+
+  // In addition to `properties`, we need to account for fractions of seconds.
+  if (this.milliseconds && filtered.indexOf('seconds') < 0) {
+    filtered.push('seconds')
+  }
+
   if (filtered.length === 0) return '0'
   return filtered
     .map(function (property) {
-      return this[property] + ' ' + property
+      var value = this[property] || 0
+
+      // Account for fractional part of seconds,
+      // remove trailing zeroes.
+      if (property === 'seconds' && this.milliseconds) {
+        value += '.' + String(this.milliseconds * 1000).replace(/[0]+$/g, '')
+      }
+
+      return value + ' ' + property
     }, this)
     .join(' ')
 }
@@ -25,7 +39,7 @@ var NUMBER = '([+-]?\\d+)'
 var YEAR = NUMBER + '\\s+years?'
 var MONTH = NUMBER + '\\s+mons?'
 var DAY = NUMBER + '\\s+days?'
-var TIME = '([+-])?([\\d]*):(\\d\\d):(\\d\\d):?(\\d\\d\\d)?'
+var TIME = '([+-])?([\\d]*):(\\d\\d):(\\d\\d)\.?(\\d{1,6})?'
 var INTERVAL = new RegExp([YEAR, MONTH, DAY, TIME].map(function (regexString) {
   return '(' + regexString + ')?'
 })
@@ -44,6 +58,12 @@ var positions = {
 // We can use negative time
 var negatives = ['hours', 'minutes', 'seconds']
 
+function parseMilliseconds (fraction) {
+  // add omitted zeroes
+  var microseconds = fraction + '000000'.slice(fraction.length)
+  return parseInt(microseconds, 10) / 1000
+}
+
 function parse (interval) {
   if (!interval) return {}
   var matches = INTERVAL.exec(interval)
@@ -54,7 +74,11 @@ function parse (interval) {
       var value = matches[position]
       // no empty string
       if (!value) return parsed
-      value = parseInt(value, 10)
+      // milliseconds are actually microseconds (up to 6 digits)
+      // with omitted trailing zeroes.
+      value = property === 'milliseconds'
+        ? parseMilliseconds(value)
+        : parseInt(value, 10)
       // no zeros
       if (!value) return parsed
       if (isNegative && ~negatives.indexOf(property)) {
